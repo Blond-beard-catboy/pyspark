963211ceae803c52a4701f7ce7fbc456c6f8e6b6
Alex <paradoxlover13@gmail.com>
2025-08-11 20:39:36 +0500

    add file for creating nwe column

---
+++ new_column.py
@@ -0,0 +1,31 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.types import *
+import pyspark.sql.functions as F
+
+# Создаем Spark сессию
+spark = SparkSession.builder \
+    .appName("Customs Data Analysis") \
+    .config("spark.local.dir", "/tmp/spark-temp") \
+    .getOrCreate()
+
+try:
+    # 1. Загрузка данных
+    df = spark.read.csv(
+        "/home/alex/pyspark/data/customs_data.csv",
+        sep=";",
+        header=True,
+        inferSchema=True
+    )
+    
+    # df.filter(F.col("netto") > 10).show()
+    df_with_netto_difference = df.withColumn("netto_info", F.when(
+        F.column("netto") > 10, F.lit("More then 10 points")        
+    ).when(
+        F.column("netto") < 10, F.lit("Less then 10 points")
+
+    ))
+
+finally:
+    spark.stop()
+    print("Spark сессия остановлена")
+
